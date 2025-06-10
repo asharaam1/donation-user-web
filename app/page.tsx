9211b237d87f425a9e4f37bfbac3e14fcf472662
ahmedhassanawan <awanhasan2007@gmail.com>
2025-06-10 16:01:07 +0500

    terms page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 // import LoginPage from './auth/Login/page';
-import About from './about/page';
+// import About from './about/page';
 // import Contact from './contact/page';
-
 import Faq from './faq/page';
+import Terms from './terms/page';
 
 const page = () => {
+  return <Terms/>
   // return <Faq />;
   // return <Contact/>;
-  return <About/>;
+  // return <About/>;
   // return <LoginPage />
 }
 
 export default page;
 
-
-/// new 
